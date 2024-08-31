1719b5e44053e0a0367a34869cd204baa248a065
Ross Buddie <r.buddie@gmail.com>
2024-08-31 01:32:55 +0100

    [MARVEL] add spec for simple comics_controller to check root path, and user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'User model' do
+    it 'should have a valid factory' do
+      expect(build(:user)).to be_valid
+    end
+
+    it 'should validate uniqueness of email' do
+      create(:user, email: 'email@example.com')
+      expect(build(:user, email: 'email@example.com')).to_not be_valid
+    end
+  end
 end
--- spec/requests/comics_spec.rb
+++ spec/requests/comics_spec.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe "Comics", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    let(:user) { create(:user) }
+    let(:root_path) { '/' }
+
+    it "returns http success" do
+      sign_in user
+
+      get root_path
+
+      expect(response.status).to eq(200)
+    end
   end
 end
